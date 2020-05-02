11f1f5471542c32766dcc9bb259892aff11c48c9
wanlab-io <hello.wanlab@gmail.com>
2020-05-02 11:21:23 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// init easyHttp library
+const http = new easyHTTP;
+
+// Get Posts
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+});
