4f530d0496451a2e98da1e4ef693eb4027621a28
wanlab-io <hello.wanlab@gmail.com>
2020-05-02 14:28:50 +0800

    add get single data

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(posts);
-  }
-});
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/27', function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+
